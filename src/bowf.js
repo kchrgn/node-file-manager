0ff0ea47633b770ed7cfc3b51d9592fe817de75e
Andrey S. Kochergin <kchrgn@gmail.com>
2022-12-13 13:13:30 +0300

    feat: implement cat command

--- src/bowf.js
+++ src/bowf.js
@@ -4,10 +4,11 @@ import * as message from './lib/message.js'
 export const catHandler = async (args) => {
 	try {
 		const fd = await open(args.join());
+		const readFileStream = fd.createReadStream({encoding: 'utf8'});
+		readFileStream.on('data', (chunk) => { console.log(chunk)});
+		readFileStream.on('end', () => message.sayCurrDir());
 	} catch (err) {
 		message.sayOperationFailed();
+		message.sayCurrDir();
 	}
-	const readFileStream = fd.createReadStream({encoding: 'utf8'});
-	readFileStream.on('data', (chunk) => { console.log(chunk)});
-	message.sayCurrDir();
 }
